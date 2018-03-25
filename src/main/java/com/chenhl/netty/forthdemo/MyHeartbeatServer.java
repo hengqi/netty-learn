77b923b00a316a1454ef0213c6ded1f7255bf530
honglei <2206812841@qq.com>
2018-03-25 19:16:50 +0800

    add about support heartbeat

---
+++ src/main/java/com/chenhl/netty/forthdemo/MyHeartbeatServer.java
@@ -0,0 +1,38 @@
+package com.chenhl.netty.forthdemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * netty 对心跳的支持
+ *
+ * 客户端可以使用聊天的客户端
+ */
+public class MyHeartbeatServer {
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO)) //针对bossGroup
+                    .childHandler(new MyHeartbeatServerInitializer());// 针对workerGroup
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
