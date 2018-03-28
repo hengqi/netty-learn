38c847b1fea100eb1db7f9319ba2112ae7177d7e
honglei <2206812841@qq.com>
2018-03-28 22:30:58 +0800

    add abount protobuf multi protocal message

--- src/main/java/com/chenhl/netty/sixthdemo/TestClientHandler.java
+++ src/main/java/com/chenhl/netty/sixthdemo/TestClientHandler.java
@@ -3,18 +3,42 @@ package com.chenhl.netty.sixthdemo;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+import java.util.Random;
+
+public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        MyDataInfo.Person person = MyDataInfo.Person.newBuilder()
-                .setName("张三").setAge(20).setAddress("杭州").build();
 
-        ctx.writeAndFlush(person);
+        int randomInt = new Random().nextInt(3);
+
+        MyDataInfo.MyMessage myMessage = null;
+
+        if (0 == randomInt) {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.PersonType)
+                    .setPerson(MyDataInfo.Person.newBuilder()
+                            .setName("张三").setAge(20).setAddress("杭州").build())
+                    .build();
+        } else if (1 == randomInt) {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.DogType)
+                    .setDog(MyDataInfo.Dog.newBuilder()
+                            .setName("一只狗").setAge(20).build())
+                    .build();
+        } else {
+            myMessage = MyDataInfo.MyMessage.newBuilder()
+                    .setDataType(MyDataInfo.MyMessage.DataType.CatType)
+                    .setCat(MyDataInfo.Cat.newBuilder()
+                            .setName("一只猫").setCity("杭州").build())
+                    .build();
+        }
+
+        ctx.writeAndFlush(myMessage);
     }
 }
