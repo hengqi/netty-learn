d372eae6ac1ea1e3355740fe9e1ff80f8b994805
chenh <chenhonglei@outlook.com>
2018-06-07 22:32:16 +0800

    add about custom protocal

---
+++ src/main/java/com/chenhl/netty/handler2/MyClientInitializer.java
@@ -0,0 +1,14 @@
+package com.chenhl.netty.handler2;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new MyClientHandler());
+    }
+}
---
+++ src/main/java/com/chenhl/netty/handler3/MyClientHandler.java
@@ -0,0 +1,44 @@
+package com.chenhl.netty.handler3;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.Charset;
+
+public class MyClientHandler extends SimpleChannelInboundHandler<PersonProtocol> {
+
+    private int count;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, PersonProtocol msg) throws Exception {
+        int length = msg.getLength();
+        byte[] content = msg.getContent();
+
+        System.out.println("客户端接收到的消息：");
+        System.out.println("长度：" + length);
+        System.out.println("内容: " + new String(content, Charset.forName("utf-8")));
+
+        System.out.println("客户端接收到的消息数量: " + (++this.count));
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            String message = "sent from client";
+            int length = message.getBytes(Charset.forName("utf-8")).length;
+            byte[] content = message.getBytes(Charset.forName("utf-8"));
+
+            PersonProtocol personProtocol = new PersonProtocol();
+            personProtocol.setLength(length);
+            personProtocol.setContent(content);
+
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
